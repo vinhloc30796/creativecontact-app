3db0f0069518d96209f2ab427272d2fbe37aba1a
Loc Nguyen <vinhloc30796@gmail.com>
2024-06-19 10:34:06 +0700

    Trim unused imports

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
