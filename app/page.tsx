b36da4f04a922b9b936a0a1709c1af3764c5a63a
Loc Nguyen <vinhloc30796@gmail.com>
2024-08-12 23:32:27 +0700

    Add debugging logs for getEventSlots

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ import { eq } from 'drizzle-orm'
 import { eventSlots } from '@/drizzle/schema'
 
 async function getEventSlots(event: string): Promise<EventSlot[]> {
+	console.log('Pulling event slots for event', event)
 	return (await db.select().from(eventSlots).where(eq(eventSlots.event, event))).map(slot => slot as EventSlot)
 }
 
 export default async function Page() {
 	const eventId = '10177076-f591-49c8-a87d-042ba7aa6345'
 	const eventSlots = await getEventSlots(eventId)
+	console.log(`Got ${eventSlots.length} event slots`);
 	return <RegisterPage eventSlots={eventSlots} />
 }
