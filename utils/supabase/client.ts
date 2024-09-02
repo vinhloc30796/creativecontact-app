f341b71449f74525e2ecd70e2ff1b2a364082c4a
Loc Nguyen <vinhloc30796@gmail.com>
2024-09-02 20:13:46 +0700

    doc!: update local environment variables
    Also, this commit upgrade the sass & supabase packages; and increase consistency in utils/supabase

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,5 +1,10 @@
-import { createBrowserClient } from '@supabase/ssr'
+// File: utils/supabase/client.ts
+
+import { createBrowserClient } from '@supabase/ssr';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export function createClient() {
-	return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+	return createBrowserClient(supabaseUrl, supabaseAnonKey)
 }
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -6,8 +6,10 @@ export async function updateSession(request: NextRequest) {
 	let supabaseResponse = NextResponse.next({
 		request,
 	})
+	const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+	const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-	const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+	const supabase = createServerClient(supabaseUrl, supabaseAnonKey, {
 		cookies: {
 			getAll() {
 				return request.cookies.getAll()
