305d4657a71783a1a2bea06efa33343d9904c396
Loc Nguyen <vinhloc30796@gmail.com>
2024-09-13 02:27:01 +0700

    feat(next-config): add image remote patterns for Supabase storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', ''),
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  nextConfig.images.remotePatterns.push({
+    protocol: 'http',
+    hostname: '127.0.0.1',
+    port: '54321',
+    pathname: '/storage/v1/object/public/**',
+  });
+}
 
 export default nextConfig;
