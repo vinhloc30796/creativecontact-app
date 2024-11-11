234db28dde4ac64e694827be58c64a64e81e7b06
duc.pham <rei.jin.71@gmail.com>
2024-11-11 16:27:01 +0700

    ui: add under contruction placeholder page
    - add Inconstruction component
    - update homepage display construction placeholde

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import RegisterPage from './(public)/(event)/register/_sections/_register'
 import { db } from '@/lib/db'
 import { eq } from 'drizzle-orm'
 import { eventSlots } from "@/drizzle/schema/event"
+import InConstruct from '@/components/InConstruction'
+import { BackgroundDiv } from '@/components/wrappers/BackgroundDiv'
+
+// show the in-construction page
+const inConstructPage = true
 
 async function getEventSlots(event: string): Promise<EventSlot[]> {
 	console.log('Pulling event slots for event', event)
@@ -13,5 +18,12 @@ export default async function Page() {
 	const eventId = '10177076-f591-49c8-a87d-042ba7aa6345'
 	const eventSlots = await getEventSlots(eventId)
 	console.log(`Got ${eventSlots.length} event slots`);
+	if (inConstructPage) {
+		return (
+			<BackgroundDiv >
+				<InConstruct />
+			</BackgroundDiv>
+		)
+	}
 	return <RegisterPage eventSlots={eventSlots} />
 }
