932e7181f9abcb1e4f7c200011fe80b23ffea1c3
Le Trung Hoa Hieu <nothisu.dev@gmail.com>
2024-12-09 14:06:08 +0700

    feat(user): add /user/[username]
    - Display only portfolio tab on /user page
    - Hide buttons on the /user page

--- app/actions/user/auth.ts
+++ app/actions/user/auth.ts
@@ -1,72 +1,99 @@
-'use server'
+"use server";
 
-import { authUsers } from "@/drizzle/schema/user"
-import { db } from '@/lib/db'
-import { createClient } from '@/utils/supabase/server'
-import { eq, isNotNull } from 'drizzle-orm'
-import { cookies } from 'next/headers'
+import { authUsers, userInfos } from "@/drizzle/schema/user";
+import { db } from "@/lib/db";
+import { createClient } from "@/utils/supabase/server";
+import { eq, isNotNull } from "drizzle-orm";
+import { cookies } from "next/headers";
 
 export async function signInAnonymously() {
-	const supabase = await createClient()
+  const supabase = await createClient();
 
-	const { data, error } = await supabase.auth.signInAnonymously()
+  const { data, error } = await supabase.auth.signInAnonymously();
 
-	if (error) {
-		console.error('Error signing in anonymously:', error)
-		return null
-	}
+  if (error) {
+    console.error("Error signing in anonymously:", error);
+    return null;
+  }
 
-	return data.user
+  return data.user;
 }
 
-export async function checkUserIsAnonymous(email: string): Promise<boolean | null> {
-	const result = await db
-		.select({ isAnonymous: authUsers.isAnonymous })
-		.from(authUsers)
-		.where(eq(authUsers.email, email))
-		.limit(1)
-
-
-	if (result.length === 0) {
-		console.error('User not found')
-		return null
-	}
-
-	const data = result[0]
-  const returning = data?.isAnonymous ?? null
-  console.log('checkUserIsAnonymous result', result, 'returning isAnonymous:', returning)
-  return returning
-}
-
-export async function checkUserEmailConfirmed(email: string): Promise<boolean | null> {
-	const result = await db
-		.select({ emailConfirmedAt: authUsers.emailConfirmedAt })
-		.from(authUsers)
-		.where(eq(authUsers.email, email))
-		.limit(1)
+export async function checkUserIsAnonymous(
+  email: string,
+): Promise<boolean | null> {
+  const result = await db
+    .select({ isAnonymous: authUsers.isAnonymous })
+    .from(authUsers)
+    .where(eq(authUsers.email, email))
+    .limit(1);
 
   if (result.length === 0) {
-    console.error('User not found')
-    return null
+    console.error("User not found");
+    return null;
+  }
+
+  const data = result[0];
+  const returning = data?.isAnonymous ?? null;
+  console.log(
+    "checkUserIsAnonymous result",
+    result,
+    "returning isAnonymous:",
+    returning,
+  );
+  return returning;
+}
+
+export async function checkUserEmailConfirmed(
+  email: string,
+): Promise<boolean | null> {
+  const result = await db
+    .select({ emailConfirmedAt: authUsers.emailConfirmedAt })
+    .from(authUsers)
+    .where(eq(authUsers.email, email))
+    .limit(1);
+
+  if (result.length === 0) {
+    console.error("User not found");
+    return null;
   } else {
-    const data = result[0]
+    const data = result[0];
     const returning = !(
       // email is not confirmed if emailConfirmedAt is null
-      data?.emailConfirmedAt === null || 
-      // email is not confirmed if emailConfirmedAt is undefined
-      data?.emailConfirmedAt === undefined
+      (
+        data?.emailConfirmedAt === null ||
+        // email is not confirmed if emailConfirmedAt is undefined
+        data?.emailConfirmedAt === undefined
+      )
     );
-    console.log('checkUserEmailConfirmed result', result, 'returning emailConfirmed: ', returning)
-    return returning
+    console.log(
+      "checkUserEmailConfirmed result",
+      result,
+      "returning emailConfirmed: ",
+      returning,
+    );
+    return returning;
   }
 }
 
 export async function getUserId(email: string): Promise<string | null> {
-	const result = await db
-		.select()
-		.from(authUsers)
-		.where(eq(authUsers.email, email))
-		.limit(1);
+  const result = await db
+    .select()
+    .from(authUsers)
+    .where(eq(authUsers.email, email))
+    .limit(1);
 
-	return result[0]?.id ?? null
+  return result[0]?.id ?? null;
+}
+
+export async function getUserIdByUsername(
+  userName: string,
+): Promise<string | null> {
+  const result = await db
+    .select()
+    .from(userInfos)
+    .where(eq(userInfos.userName, userName))
+    .limit(1);
+
+  return result[0]?.id ?? null;
 }
