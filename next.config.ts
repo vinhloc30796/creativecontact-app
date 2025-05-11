f3171b10bf3e7efa365ea87947aa31af9292518d
Loc Nguyen <vinhloc30796@gmail.com>
2025-05-11 15:13:04 +0700

    fix(nextjs/payload-cms): set img config for /payload-api

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,14 @@ import { withPayload } from '@payloadcms/next/withPayload';
 import path from 'path';
 
 const imageHost = process.env.NEXT_PUBLIC_SUPABASE_URL || '127.0.0.1:54321';
+const appHost = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
 console.log('[next.config.ts] imageHost:', imageHost);
 
 const nextConfig: NextConfig = {
   // Configure remote image patterns for Supabase storage
   images: {
     remotePatterns: [
+      // Supabase Storage
       {
         protocol: 'https',
         hostname: imageHost.replace(/^https?:\/\//, '').split(':')[0],
@@ -17,9 +19,22 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'http',
-        hostname: imageHost.replace(/^https?:\/\//, '').split(':')[0],
+        hostname: imageHost.replace(/^http?:\/\//, '').split(':')[0],
         port: '54321',
         pathname: '/storage/v1/object/public/**',
+      },
+      // Payload API Media
+      {
+        protocol: 'http',
+        hostname: appHost.replace(/^http?:\/\//, '').split(':')[0],
+        port: '3000',
+        pathname: '/payload-api/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: appHost.replace(/^https?:\/\//, '').split(':')[0],
+        port: '',
+        pathname: '/payload-api/media/**',
       },
     ],
   },
