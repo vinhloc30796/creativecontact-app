5bbd52806d9173e7d7b99c1a4cfa7b41790fcb5b
Loc Nguyen <vinhloc30796@gmail.com>
2025-09-30 11:20:42 +0700

    perf(bg): simplify loading; add webp

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Skeleton } from "@/components/ui/skeleton";
+import CreativeContactLogo, { LogoVariant } from '@/components/branding/CreativeContactLogo';
 
 export const Loading = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="space-y-4 w-[300px]">
-        <Skeleton className="h-12 w-full" />
-        <Skeleton className="h-4 w-3/4" />
-        <Skeleton className="h-4 w-1/2" />
-        <Skeleton className="h-32 w-full" />
-        <Skeleton className="h-8 w-1/3" />
+      <div className="animate-pulse">
+        <CreativeContactLogo
+          variant={LogoVariant.SHORT}
+          width={120}
+          className="opacity-75"
+        />
       </div>
     </div>
   );
